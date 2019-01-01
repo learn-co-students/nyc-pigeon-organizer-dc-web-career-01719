3f0f1da612dbd1ebbb93e1efeb3e16a49c4567ef
jameseclement <jameseclement@gmail.com>
2019-01-01 23:19:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,11 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
+  
+  ## SETS UP FINAL HASH (CURRENTLY EMPTY)
   organized_data = {}
   
+  ## CREATES KEYS FOR FINAL HASH
   data.each {|description, value|
   value.each {|array, bird|
   bird.each {|name|
@@ -10,14 +13,38 @@ def nyc_pigeon_organizer(data)
   }
   }
   }
+  
+  ## FILLS IN COLORS
+  
+  all_bird_names = organized_data.keys 
   data[:color].each {|color, names|
   names.each {|name|
-  organized_data.keys.each {|bird_name|
+  all_bird_names.each {|bird_name|
   if name === bird_name
-    organized_data[bird_name][:color] = color.to_s
+    organized_data[bird_name][:color] << color.to_s
   end
   }
   }
   }
-  binding.pry
+   ## FILLS IN GENDERS
+  data[:gender].each {|gender, names|
+  names.each {|name|
+  all_bird_names.each {|bird_name|
+  if name === bird_name
+    organized_data[bird_name][:gender] << gender.to_s
+  end
+  }
+  }
+  }
+  data[:lives].each {|city, names|
+  names.each {|name|
+  all_bird_names.each {|bird_name|
+  if name === bird_name
+    organized_data[bird_name][:lives] << city.to_s
+  end
+  }
+  }
+  }
+
+organized_data
 end

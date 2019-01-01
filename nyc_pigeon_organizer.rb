7f5ad55dad89501fc275e6374f223d8a6d819c5c
Phil Hartley <philh725@gmail.com>
2019-01-01 16:50:02 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # write your code here
+  
+  pidgeon_hash = {}
+  data.each do |attribute, att_value|
+    att_value.each do |symbol, pidgeon_name|
+      pidgeon_name.each do |i|
+        if not pidgeon_hash[i]
+          pidgeon_hash[i] = {:color => [], :gender => [], :lives => []}
+          pidgeon_hash[i][attribute] = [symbol.to_s]
+        else
+          pidgeon_hash[i].each do |key, value_array|
+            if key == attribute
+              value_array << symbol.to_s
+            end
+          end
+        end
+      end
+    end
+  end
+  pidgeon_hash
 end

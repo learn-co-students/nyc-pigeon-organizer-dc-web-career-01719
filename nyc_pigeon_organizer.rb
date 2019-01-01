d9f70a04ab924e9f26f560a970eaef358080ba58
jameseclement <jameseclement@gmail.com>
2019-01-01 16:33:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_data = {}
+  
+  data.each {|description, value|
+  value.each {|array, bird|
+  bird.each {|name|
+  organized_data[name] = {color: [], gender: [], lives: []}
+  }
+  }
+  }
+  data[:color].each {|color, names|
+  names.each {|name|
+  organized_data.keys.each {|bird_name|
+  if name === bird_name
+    organized_data[bird_name][:color] = color.to_s
+  end
+  }
+  }
+  }
+  binding.pry
 end
